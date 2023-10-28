a79d04dc7a2d89fefe08bae8d62d2547803d9921
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-10-28 09:15:11 +0200

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#355FE5',
+        secondary: '#292D41',
+        heading: '#292929',
+        content: '#71727E',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
